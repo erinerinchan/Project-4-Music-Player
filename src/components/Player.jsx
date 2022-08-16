0aeca6242962855cb49a49292e60cb44d4e36eaa
erinerinchan <17206774@life.hkbu.edu.hk>
2022-08-16 16:40:28 +0800

    Initial commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,5 @@
 import {
+  HeartIcon,
   VolumeUpIcon as VolumeDownIcon
 } from '@heroicons/react/outline'
 import {
@@ -11,7 +12,6 @@ import {
   SwitchHorizontalIcon
 } from '@heroicons/react/solid'
 import { debounce } from 'lodash'
-import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 import { useCallback, useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
@@ -28,18 +28,29 @@ function Player() {
 
   const songInfo = useSongInfo()
 
+  // console.log(songInfo);
+
   const fetchCurrentSong = () => {
     if (!songInfo) {
       spotifyApi.getMyCurrentPlayingTrack().then((data) => {
         console.log('Now playing: ', data.body?.item)
         setCurrentIdTrack(data.body?.item?.id)
 
-        spotifyApi.getMyCurrentPlaybackState().then((data) => {
-          setIsPlaying(data.body?.is_playing)
-        })
+        spotifyApi
+          .getMyCurrentPlaybackState()
+          .then((data) => setIsPlaying(data.body?.is_playing))
+          .catch(() => {})
       })
     }
   }
+
+  // Fetch the song on first load before any song has been loaded...
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong()
+      setVolume(50)
+    }
+  }, [currentTrackId, spotifyApi, session])
 
   const handlePlayPause = () => {
     spotifyApi.getMyCurrentPlaybackState().then((data) => {
@@ -52,13 +63,6 @@ function Player() {
       }
     })
   }
-
-  useEffect(() => {
-    if (spotifyApi.getAccessToken() && !currentTrackId) {
-      fetchCurrentSong()
-      setVolume(50)
-    }
-  }, [currentTrackIdState, spotifyApi, session])
 
   useEffect(() => {
     if (volume > 0 && volume < 100) {
@@ -74,56 +78,52 @@ function Player() {
   )
 
   return (
-  // Left
-    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 border-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       <div className="flex items-center space-x-4">
-        <Image
-          className="hidden md:inline"
+        <img
+          className="hidden md:inline h-10 w-10"
           src={songInfo?.album.images?.[0]?.url}
-          width="40"
-          height="40"
           alt=""
         />
         <div>
           <h3>{songInfo?.name}</h3>
           <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
+        <HeartIcon className="hidden md:inline h-5 w-5" />
       </div>
 
-      {/* Center */}
       <div className="flex items-center justify-evenly">
         <SwitchHorizontalIcon className="button" />
         <RewindIcon
-        // onClick={() => spotifyApi.skipToPrevious()} -- The API is not working
+          // onClick={() => spotifyApi.skipToPrevious()} -- The API is not working
           className="button"
         />
-
         {isPlaying ? (
-          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+          <PauseIcon className="button w-10 h-10" onClick={handlePlayPause} />
         ) : (
           <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
         )}
-
         <FastForwardIcon
+          // onClick={() => spotifyApi.skipToNext()} -- The API is not working
           className="button"
         />
         <ReplyIcon className="button" />
       </div>
 
-      {/* Right */}
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
         <VolumeDownIcon
           onClick={() => volume > 0 && setVolume(volume - 10)}
           className="button"
         />
         <input
-          className="w-14 md:w-28"
           type="range"
+          className="w-14 md:w-28"
           value={volume}
           onChange={(e) => setVolume(Number(e.target.value))}
           min={0}
           max={100}
         />
+
         <VolumeUpIcon
           onClick={() => volume < 100 && setVolume(volume + 10)}
           className="button"
